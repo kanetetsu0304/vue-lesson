5285d12c52b3530f5d978e9c5b8ba5ae5909b1c8
tetsushi-kaneko <tetsushi-kaneko@bm-sms.co.jp>
2020-06-08 12:11:24 +0900

    ToDoリストを作成しました

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <VueChild title="TODO List" />
+  </div>
+</template>
+
+<script>
+import VueChild from "./components/VueChild.vue";
+
+export default {
+  name: "App",
+  components: {
+    VueChild
+  }
+};
+</script>
---
+++ src/components/VueChild.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="vue-child" v-if="display">
+    <h1 class="vue-child_title">{{ title }}</h1>
+    <form @submit.prevent class="item-container">
+      <input type="text" v-model="newItem" />
+      <button @click="addItem()">Add</button>
+    </form>
+    <p>{{ `${area}件のタスクがあります` }}</p>
+    <ul>
+      <li v-for="(todo,index) in todos" :key="todo.id">
+        <input type="checkbox" v-model="todo.isDone" />
+        <span :class="{done:todo.isDone}">{{ todo.item }}</span>
+        <button @click="deleteItem(index)">delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueChild",
+  props: {
+    title: String
+  },
+  data: function() {
+    return {
+      newItem: "",
+      todos: [],
+      display: true
+    };
+  },
+  methods: {
+    addItem: function() {
+      const todo = {
+        item: this.newItem,
+        isDone: false
+      };
+      if (this.newItem === "") {
+        return;
+      }
+      this.todos.push(todo);
+      this.newItem = "";
+    },
+    deleteItem: function(index) {
+      this.todos.splice(index, 1);
+    }
+  },
+  computed: {
+    area: function() {
+      return this.todos.length;
+    }
+  },
+  watch: {
+    todos: {
+      handler: function() {
+        if (this.todos.length === 0) {
+          alert("タスクを全て消化しました！！！");
+        }
+      },
+      deep: true
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.vue-child {
+  width: 200px;
+  margin: 0 auto;
+}
+.vue-child ul {
+  list-style: none;
+}
+.vue-child li > span.done {
+  text-decoration: line-through;
+}
+
+.vue-child_title {
+  text-align: center;
+}
+.item-container {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
